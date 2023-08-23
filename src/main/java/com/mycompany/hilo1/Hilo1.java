49bdbdc728827acf1af5b0c816901df0ed4add9b
E5556 <e5556@hotmail.com>
2023-08-23 02:07:33 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/hilo1/Hilo1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hilo1;
+
+/**
+ *
+ * @author e5556
+ */
+public class Hilo1 {
+
+    public static void main(String[] args) {
+        Audio repro = new Audio();
+        repro.setVisible(true);
+    }
+}
